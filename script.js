5a5e9222735bb557568e0d3ec46c274fa4d54030
jvictoraraujo2002-netizen <j.victoraraujo2002@gmail.com>
2025-10-30 01:53:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('header .menu ul');
 hamburger.addEventListener('click', () => { menu.classList.toggle('active'); });
 
-// Formulário simples
-const form = document.getElementById('form-contato');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('Mensagem enviada com sucesso!');
-  form.reset();
-});
-
+// Formulário simples de contato
+const formContato = document.getElementById('form-contato');
+if(formContato){
+  formContato.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    formContato.reset();
+  });
+}
