f6ec2f3aeb37c635e28a64152577915734bd47f9
jvictoraraujo2002-netizen <j.victoraraujo2002@gmail.com>
2025-10-30 01:59:58 -0300

    Update masks.js

--- masks.js
+++ masks.js
@@ -1,31 +1,26 @@
-// Máscaras para formulário
-function maskTelefone(input) {
-  let v = input.value.replace(/\D/g, "");
-  v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
-  v = v.replace(/(\d{4,5})(\d{4})$/, "$1-$2");
-  input.value = v;
+function maskTelefone(input){
+  let v = input.value.replace(/\D/g,"");
+  v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+  v=v.replace(/(\d{4,5})(\d{4})$/,"$1-$2");
+  input.value=v;
 }
-
-function maskCPF(input) {
-  let v = input.value.replace(/\D/g, "");
-  v = v.replace(/(\d{3})(\d)/, "$1.$2");
-  v = v.replace(/(\d{3})(\d)/, "$1.$2");
-  v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
-  input.value = v;
+function maskCPF(input){
+  let v = input.value.replace(/\D/g,"");
+  v=v.replace(/(\d{3})(\d)/,"$1.$2");
+  v=v.replace(/(\d{3})(\d)/,"$1.$2");
+  v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+  input.value=v;
 }
-
-function maskCEP(input) {
-  let v = input.value.replace(/\D/g, "");
-  v = v.replace(/^(\d{5})(\d)/, "$1-$2");
-  input.value = v;
+function maskCEP(input){
+  let v = input.value.replace(/\D/g,"");
+  v=v.replace(/^(\d{5})(\d)/,"$1-$2");
+  input.value=v;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const telefoneInput = document.getElementById("telefone");
-  const cpfInput = document.getElementById("cpf");
-  const cepInput = document.getElementById("cep");
-
-  if (telefoneInput) telefoneInput.addEventListener("input", () => maskTelefone(telefoneInput));
-  if (cpfInput) cpfInput.addEventListener("input", () => maskCPF(cpfInput));
-  if (cepInput) cepInput.addEventListener("input", () => maskCEP(cepInput));
+document.addEventListener("DOMContentLoaded",()=>{
+  const tel=document.getElementById("telefone");
+  const cpf=document.getElementById("cpf");
+  const cep=document.getElementById("cep");
+  if(tel) tel.addEventListener("input",()=>maskTelefone(tel));
+  if(cpf) cpf.addEventListener("input",()=>maskCPF(cpf));
+  if(cep) cep.addEventListener("input",()=>maskCEP(cep));
 });
