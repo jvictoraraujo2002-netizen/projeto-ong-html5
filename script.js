c29bc67038c2ca256f6de0d9746aa7ed4d301b13
jvictoraraujo2002-netizen <j.victoraraujo2002@gmail.com>
2025-10-30 01:35:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Menu hambúrguer
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('header .menu ul');
+hamburger.addEventListener('click', () => { menu.classList.toggle('active'); });
+
+// Formulário simples
+const form = document.getElementById('form-contato');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Mensagem enviada com sucesso!');
+  form.reset();
+});
+
